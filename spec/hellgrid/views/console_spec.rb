d9e038b2abf3baa076917277551caf0b03df7c8b
Sasha Cooper <jinksy@gmail.com>
2016-09-12 15:39:48 +0100

    Update to Ruby 2.3.1 Heredoc syntax (2.3.0 seems bugged)

--- spec/hellgrid/views/console_spec.rb
+++ spec/hellgrid/views/console_spec.rb
@@ -13,14 +13,14 @@ describe Hellgrid::Views::Console do
 
   describe '#render_as_string' do
     it 'renders matrix' do
-      expected_result = <<-TABLE
- x |  bar  |  foo  
----+-------+-------
- b | 2.0.2 | 2.0.1 
- c | 3.0.2 | 3.0.1 
- a |   x   | 1.0.1 
- d | 4.0.2 |   x   
-TABLE
+      expected_result = <<~TABLE
+                           x |  bar  |  foo  
+                          ---+-------+-------
+                           b | 2.0.2 | 2.0.1 
+                           c | 3.0.2 | 3.0.1 
+                           a |   x   | 1.0.1 
+                           d | 4.0.2 |   x   
+                        TABLE
 
       expect(console.render_as_string).to eq(expected_result)
     end
--- spec/hellgrid_spec.rb
+++ spec/hellgrid_spec.rb
@@ -4,89 +4,89 @@ describe 'bin/hellgrid' do
   before do
     delete_tmp_folder
 
-    create_file 'spec/tmp/in/foo/Gemfile.lock', <<-FOO_GEMFILE
-GEM
-  remote: https://rubygems.org/
-  specs:
-    diff-lcs (1.2.5)
-    rake (11.1.0)
-    rspec (3.0.0)
-      rspec-core (~> 3.0.0)
-      rspec-expectations (~> 3.0.0)
-      rspec-mocks (~> 3.0.0)
-    rspec-core (3.0.4)
-      rspec-support (~> 3.0.0)
-    rspec-expectations (3.0.4)
-      diff-lcs (>= 1.2.0, < 2.0)
-      rspec-support (~> 3.0.0)
-    rspec-mocks (3.0.4)
-      rspec-support (~> 3.0.0)
-    rspec-support (3.0.4)
+    create_file 'spec/tmp/in/foo/Gemfile.lock', <<~FOO_GEMFILE
+                                                  GEM
+                                                    remote: https://rubygems.org/
+                                                    specs:
+                                                      diff-lcs (1.2.5)
+                                                      rake (11.1.0)
+                                                      rspec (3.0.0)
+                                                        rspec-core (~> 3.0.0)
+                                                        rspec-expectations (~> 3.0.0)
+                                                        rspec-mocks (~> 3.0.0)
+                                                      rspec-core (3.0.4)
+                                                        rspec-support (~> 3.0.0)
+                                                      rspec-expectations (3.0.4)
+                                                        diff-lcs (>= 1.2.0, < 2.0)
+                                                        rspec-support (~> 3.0.0)
+                                                      rspec-mocks (3.0.4)
+                                                        rspec-support (~> 3.0.0)
+                                                      rspec-support (3.0.4)
 
-PLATFORMS
-  ruby
+                                                  PLATFORMS
+                                                    ruby
 
-DEPENDENCIES
-  rake (= 11.1.0)
-  rspec (= 3.0.0)
+                                                  DEPENDENCIES
+                                                    rake (= 11.1.0)
+                                                    rspec (= 3.0.0)
 
-FOO_GEMFILE
+                                                FOO_GEMFILE
 
-    create_file 'spec/tmp/bar/Gemfile.lock', <<-BAR_GEMFILE
-GEM
-  remote: https://rubygems.org/
-  specs:
-    diff-lcs (1.2.5)
-    rake (10.0.0)
-    rspec (2.0.0)
-      rspec-core (= 2.0.0)
-      rspec-expectations (= 2.0.0)
-      rspec-mocks (= 2.0.0)
-    rspec-core (2.0.0)
-    rspec-expectations (2.0.0)
-      diff-lcs (>= 1.1.2)
-    rspec-mocks (2.0.0)
-      rspec-core (= 2.0.0)
-      rspec-expectations (= 2.0.0)
+    create_file 'spec/tmp/bar/Gemfile.lock',  <<~BAR_GEMFILE
+                                                GEM
+                                                  remote: https://rubygems.org/
+                                                  specs:
+                                                    diff-lcs (1.2.5)
+                                                    rake (10.0.0)
+                                                    rspec (2.0.0)
+                                                      rspec-core (= 2.0.0)
+                                                      rspec-expectations (= 2.0.0)
+                                                      rspec-mocks (= 2.0.0)
+                                                    rspec-core (2.0.0)
+                                                    rspec-expectations (2.0.0)
+                                                      diff-lcs (>= 1.1.2)
+                                                    rspec-mocks (2.0.0)
+                                                      rspec-core (= 2.0.0)
+                                                      rspec-expectations (= 2.0.0)
 
-PLATFORMS
-  ruby
+                                                PLATFORMS
+                                                  ruby
 
-DEPENDENCIES
-  rake (= 10.0.0)
-  rspec (= 2.0.0)
+                                                DEPENDENCIES
+                                                  rake (= 10.0.0)
+                                                  rspec (= 2.0.0)
 
-BAR_GEMFILE
+                                              BAR_GEMFILE
   end
 
   it 'returns a matrix with the versions of all used gems' do
-    expected_result = <<-TABLE
-         x          |  bar   | in/foo 
---------------------+--------+--------
-      diff-lcs      | 1.2.5  | 1.2.5  
-        rake        | 10.0.0 | 11.1.0 
-       rspec        | 2.0.0  | 3.0.0  
-     rspec-core     | 2.0.0  | 3.0.4  
- rspec-expectations | 2.0.0  | 3.0.4  
-    rspec-mocks     | 2.0.0  | 3.0.4  
-   rspec-support    |   x    | 3.0.4  
-TABLE
+    expected_result = <<~TABLE
+                                 x          |  bar   | in/foo 
+                        --------------------+--------+--------
+                              diff-lcs      | 1.2.5  | 1.2.5  
+                                rake        | 10.0.0 | 11.1.0 
+                               rspec        | 2.0.0  | 3.0.0  
+                             rspec-core     | 2.0.0  | 3.0.4  
+                         rspec-expectations | 2.0.0  | 3.0.4  
+                            rspec-mocks     | 2.0.0  | 3.0.4  
+                           rspec-support    |   x    | 3.0.4  
+                      TABLE
 
     expect(`#{PROJECT_ROOT}/bin/hellgrid #{PROJECT_ROOT}/spec/tmp`).to eq(expected_result)
   end
 
   it 'could be run from random directory' do
-    expected_result = <<-TABLE
-         x          |  bar   | in/foo 
---------------------+--------+--------
-      diff-lcs      | 1.2.5  | 1.2.5  
-        rake        | 10.0.0 | 11.1.0 
-       rspec        | 2.0.0  | 3.0.0  
-     rspec-core     | 2.0.0  | 3.0.4  
- rspec-expectations | 2.0.0  | 3.0.4  
-    rspec-mocks     | 2.0.0  | 3.0.4  
-   rspec-support    |   x    | 3.0.4  
-TABLE
+    expected_result = <<~TABLE
+                                 x          |  bar   | in/foo 
+                        --------------------+--------+--------
+                              diff-lcs      | 1.2.5  | 1.2.5  
+                                rake        | 10.0.0 | 11.1.0 
+                               rspec        | 2.0.0  | 3.0.0  
+                             rspec-core     | 2.0.0  | 3.0.4  
+                         rspec-expectations | 2.0.0  | 3.0.4  
+                            rspec-mocks     | 2.0.0  | 3.0.4  
+                           rspec-support    |   x    | 3.0.4  
+                      TABLE
 
     Bundler.with_clean_env do
       expect(`cd ~ && #{PROJECT_ROOT}/bin/hellgrid #{PROJECT_ROOT}/spec/tmp`).to eq(expected_result)
@@ -94,17 +94,17 @@ TABLE
   end
 
   it 'uses the current working directory by default' do
-    expected_result = <<-TABLE
-         x          |  bar   | in/foo 
---------------------+--------+--------
-      diff-lcs      | 1.2.5  | 1.2.5  
-        rake        | 10.0.0 | 11.1.0 
-       rspec        | 2.0.0  | 3.0.0  
-     rspec-core     | 2.0.0  | 3.0.4  
- rspec-expectations | 2.0.0  | 3.0.4  
-    rspec-mocks     | 2.0.0  | 3.0.4  
-   rspec-support    |   x    | 3.0.4  
-TABLE
+    expected_result = <<~TABLE
+                                 x          |  bar   | in/foo 
+                        --------------------+--------+--------
+                              diff-lcs      | 1.2.5  | 1.2.5  
+                                rake        | 10.0.0 | 11.1.0 
+                               rspec        | 2.0.0  | 3.0.0  
+                             rspec-core     | 2.0.0  | 3.0.4  
+                         rspec-expectations | 2.0.0  | 3.0.4  
+                            rspec-mocks     | 2.0.0  | 3.0.4  
+                           rspec-support    |   x    | 3.0.4  
+                      TABLE
 
     Bundler.with_clean_env do
       expect(`cd #{PROJECT_ROOT}/spec/tmp && #{PROJECT_ROOT}/bin/hellgrid`).to eq(expected_result)
